d69be593bded6d08ca5b974f939173637061ce56
Reza <mrdev1020@gmail.com>
2017-11-29 22:17:10 +0330

    Update Cookie.cs

--- Cookie.cs
+++ Cookie.cs
@@ -1,20 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
-namespace MohammadReza
+namespace Providers
 {
-    public class Cookie : Controller
+    public class Cookie
     {
-        /*  Set Cookie
-        *
-        *   @param String Name
-        *   @param String Value
-        *   @param String Date
-        *   @param String Time
-        */
+
         public void SetCookie(string name, string value, string date, int time)
         {
             HttpCookie cookie = new HttpCookie(name, value);
@@ -46,22 +39,13 @@ namespace MohammadReza
 
 
 
-        /*  Get Cookie
-        *
-        *   @param String Name
-        *   @return String
-        */            
         public string GetCookie(string name)
         {
             return System.Web.HttpContext.Current.Request.Cookies[name].Value.ToString();
         }
 
 
-        /*  Exist Cookie
-        *
-        *   @param String Name
-        *   @return Bool
-        */
+
         public bool ExistCookie(string name)
         {
             HttpCookie cookie = System.Web.HttpContext.Current.Request.Cookies[name];
@@ -77,13 +61,11 @@ namespace MohammadReza
         }
 
 
-        /*  Remove Cookie
-        *
-        *   @param String Name
-        */
+
         public void RemoveCookie(string name)
         {
             System.Web.HttpContext.Current.Request.Cookies.Remove(name);
         }
     }
+
 }
